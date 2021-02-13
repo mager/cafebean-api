d0e6be0c793b9bf5c3a812cf0f485efdc0a39db5
Andrew Mager <andrew+github@mager.co>
2021-02-13 16:10:25 -0500

    Move to Cloud Run (#3)

--- Tiltfile
+++
@@ -1,3 +0,0 @@
-docker_build('caffy-beans-api', '.', dockerfile='Dockerfile')
-k8s_yaml('deployment.yaml')
-k8s_resource('caffy-beans-api', port_forwards=8080)
--- main.go
+++ main.go
@@ -6,11 +6,10 @@ import (
 
 	"cloud.google.com/go/firestore"
 	"github.com/gorilla/mux"
-	"github.com/mager/caffy-beans/config"
-	"github.com/mager/caffy-beans/database"
-	"github.com/mager/caffy-beans/handler"
-	"github.com/mager/caffy-beans/logger"
-	"github.com/mager/caffy-beans/router"
+	"github.com/mager/cafebean-api/database"
+	"github.com/mager/cafebean-api/handler"
+	"github.com/mager/cafebean-api/logger"
+	"github.com/mager/cafebean-api/router"
 	"go.uber.org/fx"
 	"go.uber.org/zap"
 )
@@ -18,7 +17,6 @@ import (
 func main() {
 	fx.New(
 		fx.Provide(
-			config.Options,
 			database.Options,
 			router.Options,
 			logger.Options,
@@ -27,18 +25,18 @@ func main() {
 	).Run()
 }
 
+// Register registers all of the lifecycle methods and involkes the handler
 func Register(
 	lifecycle fx.Lifecycle,
 	database *firestore.Client,
-	cfg *config.Config,
 	logger *zap.SugaredLogger,
 	router *mux.Router,
 ) {
 	lifecycle.Append(
 		fx.Hook{
 			OnStart: func(context.Context) error {
-				logger.Info("Listening on ", cfg.Application.Address)
-				go http.ListenAndServe(cfg.Application.Address, router)
+				logger.Info("Listening on :8080")
+				go http.ListenAndServe(":8080", router)
 				return nil
 			},
 			OnStop: func(context.Context) error {
