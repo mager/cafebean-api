75b97eb8fe6e272c514b8b26d58bc888a3aa109d
Mager <andrew.mager@gmail.com>
2021-04-25 08:53:10 -0500

    Temp stop db

--- handler/get_reviews.go
+++ handler/get_reviews.go
@@ -1,12 +1,8 @@
 package handler
 
 import (
-	"context"
 	"encoding/json"
 	"net/http"
-	"time"
-
-	"cloud.google.com/go/firestore"
 )
 
 // GetReviewsResp is the response for the GET /reviews endpoint
@@ -16,74 +12,74 @@ type GetReviewsResp struct {
 
 func (h *Handler) getReviews(w http.ResponseWriter, r *http.Request) {
 	var (
-		resp     = &GetReviewsResp{}
-		ctx      = context.TODO()
-		beanDocs = []*firestore.DocumentRef{}
-		reviews  []ReviewWithBean
-		beans    = h.database.Collection("beans")
+		resp = &GetReviewsResp{}
+		// ctx      = context.TODO()
+		// beanDocs = []*firestore.DocumentRef{}
+		// reviews  []ReviewWithBean
+		// beans    = h.database.Collection("beans")
 	)
 
 	// Call Postgres
-	rows, err := h.postgres.Query(`
-		SELECT
-			r.review_id,
-			r.rating,
-			r.review,
-			r.bean_ref,
-			r.updated_at,
-			u.username as user
-		FROM reviews r
-		LEFT JOIN users u on r.user_id = u.user_id;
-	`)
-	if err != nil {
-		h.logger.Error(err)
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var reviewId int
-		var rating float64
-		var review string
-		var beanRef string
-		var updatedAt time.Time
-		var user string
-		err = rows.Scan(&reviewId, &rating, &review, &beanRef, &updatedAt, &user)
-		if err != nil {
-			h.logger.Error(err)
-		}
-		h.logger.Infow(
-			"Result from Postgres",
-			"reviewId", reviewId,
-			"beanRef", beanRef,
-			"review", review,
-			"rating", rating,
-			"updatedAt", updatedAt,
-			"user", user,
-		)
+	// rows, err := h.postgres.Query(`
+	// 	SELECT
+	// 		r.review_id,
+	// 		r.rating,
+	// 		r.review,
+	// 		r.bean_ref,
+	// 		r.updated_at,
+	// 		u.username as user
+	// 	FROM reviews r
+	// 	LEFT JOIN users u on r.user_id = u.user_id;
+	// `)
+	// if err != nil {
+	// 	h.logger.Error(err)
+	// }
+	// defer rows.Close()
+	// for rows.Next() {
+	// 	var reviewId int
+	// 	var rating float64
+	// 	var review string
+	// 	var beanRef string
+	// 	var updatedAt time.Time
+	// 	var user string
+	// 	err = rows.Scan(&reviewId, &rating, &review, &beanRef, &updatedAt, &user)
+	// 	if err != nil {
+	// 		h.logger.Error(err)
+	// 	}
+	// 	h.logger.Infow(
+	// 		"Result from Postgres",
+	// 		"reviewId", reviewId,
+	// 		"beanRef", beanRef,
+	// 		"review", review,
+	// 		"rating", rating,
+	// 		"updatedAt", updatedAt,
+	// 		"user", user,
+	// 	)
 
-		reviews = append(reviews, ReviewWithBean{
-			Review:    review,
-			Rating:    rating,
-			UpdatedAt: updatedAt,
-			User:      user,
-		})
-		beanDocs = append(beanDocs, beans.Doc(beanRef))
-	}
-	err = rows.Err()
-	if err != nil {
-		h.logger.Error(err)
-	}
+	// 	reviews = append(reviews, ReviewWithBean{
+	// 		Review:    review,
+	// 		Rating:    rating,
+	// 		UpdatedAt: updatedAt,
+	// 		User:      user,
+	// 	})
+	// 	beanDocs = append(beanDocs, beans.Doc(beanRef))
+	// }
+	// err = rows.Err()
+	// if err != nil {
+	// 	h.logger.Error(err)
+	// }
 
-	// Fetch related beans
-	beanSnaps, err := h.database.GetAll(ctx, beanDocs)
-	if err != nil {
-		// TODO: Handle error.
-		h.logger.Error(err)
-	}
+	// // Fetch related beans
+	// beanSnaps, err := h.database.GetAll(ctx, beanDocs)
+	// if err != nil {
+	// 	// TODO: Handle error.
+	// 	h.logger.Error(err)
+	// }
 
-	for i, review := range reviews {
-		review.Bean = docToBean(beanSnaps[i])
-		resp.Reviews = append(resp.Reviews, review)
-	}
+	// for i, review := range reviews {
+	// 	review.Bean = docToBean(beanSnaps[i])
+	// 	resp.Reviews = append(resp.Reviews, review)
+	// }
 
 	json.NewEncoder(w).Encode(resp)
 }
--- postgres/postgres.go
+++ postgres/postgres.go
@@ -2,46 +2,43 @@ package postgres
 
 import (
 	"database/sql"
-	"fmt"
-	"log"
 
-	"github.com/kelseyhightower/envconfig"
 	_ "github.com/lib/pq"
-	"github.com/mager/cafebean-api/config"
 )
 
 // ProvidePostgres provides a postgres client
 func ProvidePostgres() *sql.DB {
 	// Initialize config
-	var conf config.Config
+	// var conf config.Config
 
-	err := envconfig.Process("cafebean", &conf)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	var (
-		host     = conf.PostgresHostname
-		port     = 5432
-		user     = "postgres"
-		password = conf.PostgresPassword
-		dbname   = "postgres"
-	)
+	// err := envconfig.Process("cafebean", &conf)
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// }
+	// var (
+	// 	host     = conf.PostgresHostname
+	// 	port     = 5432
+	// 	user     = "postgres"
+	// 	password = conf.PostgresPassword
+	// 	dbname   = "postgres"
+	// )
 
-	psqlInfo := fmt.Sprintf(
-		"host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		panic(err)
-	}
-	// defer db.Close()
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Successfully connected to Postgres db (%s)\n", host)
+	// psqlInfo := fmt.Sprintf(
+	// 	"host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable",
+	// 	host, port, user, password, dbname)
+	// db, err := sql.Open("postgres", psqlInfo)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// // defer db.Close()
+	// err = db.Ping()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Printf("Successfully connected to Postgres db (%s)\n", host)
 
-	return db
+	// return db
+	return &sql.DB{}
 }
 
 var Options = ProvidePostgres
