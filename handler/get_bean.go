f43993fef111648229e24f3965335f8b581a7c07
Mager <andrew.mager@gmail.com>
2021-04-25 09:30:12 -0500

    Handle review on bean

--- handler/get_bean.go
+++ handler/get_bean.go
@@ -6,8 +6,8 @@ import (
 	"net/http"
 	"time"
 
+	"cloud.google.com/go/firestore"
 	"github.com/gorilla/mux"
-	"google.golang.org/api/iterator"
 )
 
 // GetBeanResp is the response for the GET /bean/{slug} endpoint
@@ -18,16 +18,18 @@ type GetBeanResp struct {
 
 func (h *Handler) getBean(w http.ResponseWriter, r *http.Request) {
 	var (
-		ctx  = context.TODO()
-		resp = &GetBeanResp{}
-		vars = mux.Vars(r)
-		slug = vars["slug"]
+		ctx     = context.TODO()
+		resp    = &GetBeanResp{}
+		vars    = mux.Vars(r)
+		slug    = vars["slug"]
+		beanDoc *firestore.DocumentSnapshot
+		err     error
+		reviews []Review
 	)
-
 	// Get the bean
 	beanIter := h.database.Collection("beans").Where("slug", "==", slug).Documents(ctx)
 	for {
-		beanDoc, err := beanIter.Next()
+		beanDoc, err = beanIter.Next()
 		if beanDoc == nil {
 			http.Error(w, "invalid bean", http.StatusBadRequest)
 			return
@@ -42,28 +44,60 @@ func (h *Handler) getBean(w http.ResponseWriter, r *http.Request) {
 		break
 	}
 
-	// Get reviews
-	q := h.database.Collection("reviews").Where("bean", "==", slug)
-	iter := q.Documents(ctx)
-	defer iter.Stop()
+	h.logger.Info(beanDoc.Ref.ID)
 
-	for {
-		doc, err := iter.Next()
-		if err == iterator.Done {
-			break
-		}
+	// Get reviews
+	rows, err := h.postgres.Query(`
+		SELECT
+			r.review_id,
+			r.rating,
+			r.review,
+			r.bean_ref,
+			r.updated_at,
+			u.username as user
+		FROM reviews r
+		LEFT JOIN users u on r.user_id = u.user_id
+		WHERE r.bean_ref = $1;
+	`, beanDoc.Ref.ID)
+	if err != nil {
+		h.logger.Error(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var reviewId int
+		var rating float64
+		var review string
+		var beanRef string
+		var updatedAt time.Time
+		var user string
+		err = rows.Scan(&reviewId, &rating, &review, &beanRef, &updatedAt, &user)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
+			h.logger.Error(err)
 		}
-		data := doc.Data()
-		resp.Reviews = append(resp.Reviews, Review{
-			Rating:    data["rating"].(float64),
-			Review:    data["review"].(string),
-			UpdatedAt: data["updated_at"].(time.Time),
-			User:      data["user"].(string),
+		h.logger.Infow(
+			"Result from Postgres",
+			"reviewId", reviewId,
+			"beanRef", beanRef,
+			"review", review,
+			"rating", rating,
+			"updatedAt", updatedAt,
+			"user", user,
+		)
+
+		reviews = append(reviews, Review{
+			Review:    review,
+			Rating:    rating,
+			UpdatedAt: updatedAt,
+			User:      user,
+			Bean:      slug,
 		})
 	}
+	err = rows.Err()
+	if err != nil {
+		h.logger.Error(err)
+	}
+
+	resp.Reviews = reviews
 
 	json.NewEncoder(w).Encode(resp)
 }
